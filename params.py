6cab27a7362dd11381a7d0535269aa291eaef081
brianleecheowteng <brianleecheowteng@gmail.com>
2021-06-29 13:13:43 +0800

    Adjust default parameter for watchlist

--- params.py
+++ params.py
@@ -1,7 +1,7 @@
 intervals = ['1s', '5s', '15s', '30s', '1m','5m']
 outlier_param = {'1s':0.01,'5s':0.03,'15s':0.05,'30s':0.06,'1m':0.07,'5m':0.10}
 pairs_of_interest = ['USDT','BTC']
-watchlist = ['ADAUSDT']
+watchlist = []  # E.g. ['ADAUSDT', 'ETHUSDT']
 
 # Used for telegram bot updates
 token = ''
