2f28f8bd0b2c605f061d4f450a8eb8e99584ef35
brianleecheowteng <brianleecheowteng@gmail.com>
2021-06-29 13:11:46 +0800

    Added parameters for telegram integration

--- params.py
+++ params.py
@@ -1,2 +1,8 @@
-intervals = ['1s', '5s', '15s', '30s', '1m','15m']
-outlier_param = {'1s':0.01,'5s':0.03,'15s':0.05,'30s':0.06,'1m':0.07,'15m':0.10}
+intervals = ['1s', '5s', '15s', '30s', '1m','5m']
+outlier_param = {'1s':0.01,'5s':0.03,'15s':0.05,'30s':0.06,'1m':0.07,'5m':0.10}
+pairs_of_interest = ['USDT','BTC']
+watchlist = ['ADAUSDT']
+
+# Used for telegram bot updates
+token = ''
+chat_id = '' # Refer to chatid
