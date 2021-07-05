562576036010c7589bc9104daaa1d02279b59cc5
brianleecheowteng <brianleecheowteng@gmail.com>
2021-07-05 20:11:13 +0800

    Default FUTURES set to false

--- params.py
+++ params.py
@@ -2,7 +2,7 @@ intervals = ['1s', '5s', '15s', '30s', '1m']
 outlier_param = {'1s':0.01,'5s':0.03,'15s':0.05,'30s':0.06,'1m':0.07,'5m':0.10}
 pairs_of_interest = ['USDT']
 watchlist = []  # E.g. ['ADAUSDT', 'ETHUSDT']
-FUTURE_ENABLED=True
+FUTURE_ENABLED=False
 
 # Used for telegram bot updates
 token = ''  # Insert token obtained from @BotFather here
