1ecf977460d53893be02194af352e7cd48c83a92
brianleecheowteng <brianleecheowteng@gmail.com>
2021-07-05 20:09:16 +0800

    Add parameter for futures monitoring

--- params.py
+++ params.py
@@ -1,8 +1,9 @@
-intervals = ['1s', '5s', '15s', '30s', '1m','5m']
+intervals = ['1s', '5s', '15s', '30s', '1m']
 outlier_param = {'1s':0.01,'5s':0.03,'15s':0.05,'30s':0.06,'1m':0.07,'5m':0.10}
-pairs_of_interest = ['USDT','BTC']
+pairs_of_interest = ['USDT']
 watchlist = []  # E.g. ['ADAUSDT', 'ETHUSDT']
+FUTURE_ENABLED=True
 
 # Used for telegram bot updates
-token = ''
-chat_id = '' # Refer to chatid
+token = ''  # Insert token obtained from @BotFather here
+chat_id = 0  # Refer to chatid
