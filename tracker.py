a42851de0946fba224d023b63c8516111c84edae
brianleecheowteng <brianleecheowteng@gmail.com>
2021-07-22 01:25:39 +0800

    [QOL] Remove old file

--- tracker.py
+++
@@ -1,9 +0,0 @@
-import requests
-import time
-
-start= time.time()
-response = requests.get("https://api.binance.com/api/v3/ticker/bookTicker?symbol=TORNUSDT")
-data = response.json()
-
-print(data)
-print("Time taken:",time.time()-start)
