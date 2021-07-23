acaa73f869ff3865d21db97b52aa13e8b9ea4f9c
brianleecheowteng <brianleecheowteng@gmail.com>
2021-07-23 10:09:13 +0800

    [QOL] Reset_interval set to '12h' + Top Pump Tele

--- params.py
+++ params.py
@@ -1,15 +1,19 @@
+# Main params
 intervals = ['1s','5s','15s', '30s', '1m','15m','30m','1h']
 outlier_param = {'1s':0.02,'5s':0.03,'15s':0.06,'30s':0.08,'1m':0.1,'5m':0.10,'15m':0.15,'30m':0.20,'1h':0.30}
 pairs_of_interest = ['USDT'] # Other options include 'BTC' , 'ETH'
+
+# Optional Watchlist only mode
 watchlist = []  # E.g. ['ADAUSDT', 'ETHUSDT'] # Note that if watchlist has pairs, ONLY pairs in watchlist will be monitored
 
 # Feature params
 FUTURE_ENABLED=False # Determine whether to look at future markets
 DUMP_ENABLED = True # Determine whether to look at DUMP
 
+# Top Pump & Dump Feature Params
 TOP_PUMP_ENABLED = True # Set to false if not interested in top pump info
 TOP_DUMP_ENABLED = True # Set to false if not interested in top dump info
-TOP_PUMP_DUMP_ALERT_INTERVAL = '1h' # Interval for information to be sent
+TOP_PUMP_DUMP_ALERT_INTERVAL = '3h' # Interval for information to be sent
 VIEW_NUMBER = 5 # Top X amount of coins shown, adjust to show more or less within the timeframe
 
 
@@ -19,10 +23,11 @@ EXTRACT_INTERVAL = '1s'  # Interval between each price extract
 
 # Debug Params (Avoid touching it if there's no issues)
 PRINT_DEBUG = True # If false we do not print messages
-RESET_INTERVAL = '3h' # Interval for clearing array to prevent MEM ERROR can handle up to 12h+ depending on system
+RESET_INTERVAL = '12h' # Interval for clearing array to prevent MEM ERROR can handle up to 12h+ depending on system
 GET_PRICE_FAIL_INTERVAL = '1s' # In the case of get price fail, this is the time delay before re-attempt
 SEND_TELEGRAM_FAIL_INTERVAL = '1s' # If telegram message fails to send, this is the time delay before re-attempt
 
 # Used for telegram bot updates
 token = ''  # Insert token obtained from @BotFather here
 chat_id = 0 # Insert Chat ID
+tpdpa_chat_id = 0 # Insert Chat ID for top pump dump alert, if left at 0 it'll send to chat_id
